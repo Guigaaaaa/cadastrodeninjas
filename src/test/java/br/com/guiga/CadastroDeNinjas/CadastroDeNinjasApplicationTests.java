30458477e1fb325c57b80e1147cf01c166bd452e
Guilherme Dumont <guidumontdtna@gmail.com>
2026-01-12 13:10:56 -0300

    first commit

---
+++ src/test/java/br/com/guiga/CadastroDeNinjas/CadastroDeNinjasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.guiga.CadastroDeNinjas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeNinjasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
