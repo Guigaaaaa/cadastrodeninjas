36db22d04e1a189aa1d14cb14d18c7b2e0c81217
Guilherme Dumont <guidumontdtna@gmail.com>
2026-01-16 14:49:12 -0300

    feat: cria as rotas do controller com mock

--- src/main/java/br/com/guiga/CadastroDeNinjas/missoes/MissoesController.java
+++ src/main/java/br/com/guiga/CadastroDeNinjas/missoes/MissoesController.java
@@ -1,4 +1,28 @@
 package br.com.guiga.CadastroDeNinjas.missoes;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("missoes")
 public class MissoesController {
+
+    @GetMapping("/listar")
+    public String listarMissao() {
+        return "Missões listadas com sucesso";
+    }
+
+    @PostMapping("criar")
+    public String criarMissao() {
+        return "Missão criada com sucesso";
+    }
+
+    @PutMapping("/alterar")
+    public String alterarMissao() {
+        return "Missão alterada com sucesso";
+    }
+
+    @DeleteMapping("/deletar")
+    public String deletarMissao() {
+        return "Missão deletada com sucesso";
+    }
 }
