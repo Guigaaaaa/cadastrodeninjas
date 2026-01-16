2cd547e2b80d1b6e9faf9b7dfcb94c169219d7f8
Guilherme Dumont <guidumontdtna@gmail.com>
2026-01-16 17:27:12 -0300

    Chore: inclui a versão do DTO pela entitade e mapeia manualmente no NinjaMapper os caminhos entidade x dto

--- src/main/java/br/com/guiga/CadastroDeNinjas/ninjas/NinjaDTO.java
+++ src/main/java/br/com/guiga/CadastroDeNinjas/ninjas/NinjaDTO.java
@@ -1,4 +1,18 @@
 package br.com.guiga.CadastroDeNinjas.ninjas;
 
+import br.com.guiga.CadastroDeNinjas.missoes.MissoesModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NinjaDTO {
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+    private MissoesModel missoes;
+    private String rank;
 }
